588fb0b2ddd18910025cb85d6109d9b18171a3e5
Vimal Grace M <vimalgracerobotics@gmail.com>
2023-12-12 13:27:39 +0530

    some changes

--- converter_pkg/converter_pkg/nav2_cmd_to_arduino.py
+++ converter_pkg/converter_pkg/nav2_cmd_to_arduino.py
@@ -8,6 +8,7 @@ class Nav2ToArduinoConverter(Node):
     def __init__(self):
         super().__init__("arduino_cmd_vel")
         self.nav2_cmd_subscriber_ = self.create_subscription(Twist,"cmd_vel_nav",self.callback_nav2_cmd_vel,10)
+        # self.nav2_cmd_subscriber_ = self.create_subscription(Twist,"cmd_vel",self.callback_nav2_cmd_vel,10)
         self.arduino_cmd_publisher = self.create_publisher(Twist,"/diffbot_base_controller/cmd_vel_unstamped",10)
 
     def callback_nav2_cmd_vel(self,cmd_data):
